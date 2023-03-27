0375213b817e4eb8ccf6e68ca605b42037611a71
VindexTOS <giorgikutateladze1998@gmail.com>
2023-03-27 20:14:57 +0400

    zoom in added

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react'
 import { SiNetlify } from 'react-icons/si'
-import { AiFillYoutube, AiFillGithub } from 'react-icons/ai'
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineZoomIn,
+  AiOutlineZoomOut,
+} from 'react-icons/ai'
 import { IconBaseProps, IconType } from 'react-icons'
+import { BsArrowLeftCircle, BsArrowRightCircle } from 'react-icons/bs'
+
 type ProjectCardProps = {
   title: string
   dec: string
@@ -20,24 +27,99 @@ const ProjectCard: FC<ProjectCardProps> = ({
   imgs,
   icons,
 }) => {
+  // zoom in img
+
+  const [zoom, setZoom] = React.useState<boolean>(false)
+
   const style = {
-    mainDiv: `w-[400px] h-[600px] border-2 border-white  bg-white p-5 flex flex-col items-center `,
-    img: `outline outline-2 outline-blue-200 `,
+    mainDiv: `w-[400px] h-[600px] border-2 border-white  bg-gray-800 p-5 flex flex-col items-center `,
+    img: `z-20 outline outline-2 outline-blue-200  ${
+      zoom ? 'w-[700px] h-[500px]  absolute' : ' '
+    }`,
     linkDiV: `w-[100%] `,
     innerLinks: ` flex items-center justify-starts gap-2`,
     links: ` underline text-blue-500 hover:text-blue-600`,
     iconDiv: `flex p-2 w-[100%] text-[2rem] `,
+    imgDiv: `pb-2  ${zoom ? '' : 'relative '}`,
+    arrowDiv: ` ${
+      zoom
+        ? 'text-[3rem] w-[700px]   flex justify-between items-center'
+        : 'absolute flex w-[100%] justify-between px-2 text-[2rem] items-center h-[100%]'
+    }`,
+    arrowIcon: `text-gray-300 hover:text-gray-400  cursor-pointer`,
   }
+  const [imgIndex, setImgIndex] = React.useState<number>(0)
+  // img scroller
+  const ArrowMovment = (direction: string) => {
+    if (direction === 'right') {
+      if (imgIndex >= imgs.length - 1) {
+        setImgIndex(0)
+      } else {
+        setImgIndex(imgIndex + 1)
+      }
+    } else if (direction === 'left') {
+      if (imgIndex <= 0) {
+        setImgIndex(0)
+      } else {
+        setImgIndex(imgIndex - 1)
+      }
+    }
+  }
+  // img scrolling by itself
+  React.useEffect(() => {
+    const time = setTimeout(() => {
+      if (imgIndex >= imgs.length - 1) {
+        setImgIndex(0)
+      } else {
+        setImgIndex(imgIndex + 1)
+      }
+    }, 3000)
+    return () => clearInterval(time)
+  }, [imgIndex])
+
   return (
     <div className={style.mainDiv}>
       <h1 className="text-blue-400 text-[2rem] font-bold">{title}</h1>
       <div className={style.iconDiv}>
-        {' '}
         {icons?.map((val: any) => (
           <p>{val}</p>
         ))}
       </div>
-      <img className={style.img} src={imgs[0]} />
+      <div className={style.imgDiv}>
+        <div className={style.arrowDiv}>
+          <BsArrowLeftCircle
+            onClick={() => ArrowMovment('left')}
+            className={style.arrowIcon}
+          />
+          <BsArrowRightCircle
+            onClick={() => ArrowMovment('right')}
+            className={style.arrowIcon}
+          />
+        </div>
+        {zoom ? (
+          <AiOutlineZoomOut
+            className="  text-[2rem] text-blue-400"
+            onClick={() => setZoom(!zoom)}
+          />
+        ) : (
+          <AiOutlineZoomIn
+            onClick={() => setZoom(!zoom)}
+            className="absolute text-[2rem] text-blue-400"
+          />
+        )}
+        <img className={style.img} src={imgs[imgIndex]} />
+
+        <div className="flex w-[100%] absolute items-center justify-center gap-5 bottom-5  ">
+          {imgs.map((val: any, index: number) => (
+            <div
+              className={`w-[10px] h-[10px] rounded-[50%]   ${
+                index === imgIndex ? 'bg-yellow-300' : 'bg-blue-300'
+              }`}
+            ></div>
+          ))}
+        </div>
+      </div>
+
       <div className={style.linkDiV}>
         <div className={style.innerLinks}>
           <SiNetlify className="text-blue-400" />
@@ -58,7 +140,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
           </a>
         </div>
       </div>
-      <p className="text-[12px]">{dec}</p>
+      <p className="text-[12px] text-white">{dec}</p>
     </div>
   )
 }
